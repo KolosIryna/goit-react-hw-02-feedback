bb9382219581329e6f5cef0099656618ff7e732b
KolosIryna <127253667+KolosIryna@users.noreply.github.com>
2023-10-01 19:12:34 +0200

    feedback-list

--- src/components/Feedback/FeedbackList.jsx
+++ src/components/Feedback/FeedbackList.jsx
@@ -1,7 +1,9 @@
 import { Component } from 'react';
 
-import { Buttons } from '../Buttons/Buttons';
-import { Statistics } from '../Statistics/Statistics';
+import { Section } from './Section/Section';
+import { Buttons } from './Buttons/Buttons';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
 
 import css from './FeedbackList.module.css';
 
@@ -20,19 +22,44 @@ export class FeedbackList extends Component {
     });
   };
 
+  //  Сума загальних відгуків
+  countTotalFeedback = () =>
+    this.state.good + this.state.neutral + this.state.bad;
+
+  // Відсоток позитивних відгуків
+  countPositiveFeedbackPercentage = () => {
+    const totalFeedback = this.countTotalFeedback();
+    return totalFeedback > 0
+      ? Math.round((this.state.good / totalFeedback) * 100)
+      : 0;
+  };
+
   render() {
     const keys = Object.keys(this.state);
+    const totalFeedback = this.countTotalFeedback();
+
     return (
-      <section>
+      <div>
         <h1 className={css.title}>Please leave feedback</h1>
-        <Buttons keys={keys} onClickButton={this.onClickButton} />
-        <h2 className={css.title}>Statistics</h2>
-        <Statistics
-          good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-        />
-      </section>
+
+        <Section title="Feedback Options">
+          <Buttons keys={keys} onClickButton={this.onClickButton} />
+        </Section>
+
+        <Section title="Statistics">
+          {totalFeedback > 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={totalFeedback}
+              percentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </div>
     );
   }
 }
